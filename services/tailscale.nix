d0c616445efdcb520e70def79ad5c3cd939c9437
l1npengtul <l1npengtul@protonmail.com>
2026-01-24 05:57:05 +0900

    l1npengtul@clubcyberia: Sat Jan 24 05:57:05 AM JST 2026 -

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -23,52 +23,15 @@ in {
       ]
       else [pkgs.tailscale];
 
-    services.tailscale.enable = true;
-    services.tailscale.useRoutingFeatures = cfg.side;
+    services.tailscale = {
+      enable = true;
+      useRoutingFeatures = cfg.side;
+      authKeyFile = config.sops.secrets."tailscale_key".path;
+    };
 
     sops.secrets."tailscale_key" = {
       sopsFile = ./. + "/../secrets/${config.networking.hostName}.yaml";
       owner = "tailscale-autoconnect";
-    };
-
-    systemd.services.tailscale-autoconnect = {
-      description = "Automatic connection to Tailscale";
-
-      # make sure tailscale is running before trying to connect to tailscale
-      after = [
-        "network-pre.target"
-        "tailscale.service"
-        "sops-nix.service"
-      ];
-      wants = [
-        "network-pre.target"
-        "tailscale.service"
-      ];
-      wantedBy = ["multi-user.target"];
-
-      # set this service as a oneshot job
-      serviceConfig = {
-        User = "tailscale-autoconnect";
-        Type = "oneshot";
-      };
-
-      # have the job run this shell script
-      script = with pkgs; ''
-        # wait for tailscaled to settle
-        sleep 2
-
-        # check if we are already authenticated to tailscale
-        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-          exit 0
-        fi
-
-        export TSKEY=$(cat ${config.sops.secrets."tailscale_key".path})
-
-        # otherwise authenticate with tailscale
-        ${tailscale}/bin/tailscale up -authkey
-
-      '';
     };
 
     networking.firewall = {
