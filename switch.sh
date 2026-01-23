3223fb8824befbd61d87d0e1f9e1da386f7a30e2
l1npengtul <l1npengtul@protonmail.com>
2026-01-24 05:19:39 +0900

    l1npengtul@clubcyberia: Sat Jan 24 05:19:39 AM JST 2026 -

--- switch.sh
+++ switch.sh
@@ -11,6 +11,9 @@ git add .
 
 git commit  --allow-empty -m "$(whoami)@${WHERE}: $(date) - $@"
 
+export SSH_TO_AGE_PASSPHRASE=$(systemd-ask-password)
+export SOPS_AGE_KEY=$(ssh-to-age -i ~/.ssh/id_ed25519 -private-key)
+
 sudo nixos-rebuild switch --show-trace --builders "" --flake .#$WHERE
 
 nix-env --delete-generations 7d
