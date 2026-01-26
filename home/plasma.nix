3682712a2c5ce5008d516cdf70d66652803e77a8
l1npengtul <l1npengtul@protonmail.com>
2026-01-27 07:00:45 +0900

    l1npengtul@clubcyberia: Tue Jan 27 07:00:45 AM JST 2026 -

--- home/plasma.nix
+++ home/plasma.nix
@@ -1,18 +1,16 @@
 {
-  xdg.dataFile = {
-    icons = {
-      "Chicago95".source = ./Chicago95/Icons/Chicago95;
-      "miku-cursor-linux".source = ./hatsune-miku-windows-linux-cursors/miku-cursor-linux;
-    };
-    "color-schemes/PlasmaOverdose.colors".source = ./Plasma-Overdose/colorschemes/PlasmaOverdose.colors;
-    "sounds/Plasma-Overdose".source = ./Plasma-Overdose/sounds;
-    "aurorae/themes/Plasma-Overdose" = {
-      recursive = true;
-      source = ./Plasma-Overdose/aurorae/Plasma-Overdose;
-    };
-    "plasma/look-and-feel/Plasma-Overdose".source =
-      ./Plasma-Overdose/plasma/look-and-feel/Plasma-Overdose;
+  xdg.dataFile."icons/Chicago95".source = ./Chicago95/Icons/Chicago95;
+  xdg.dataFile."icons/miku-cursor-linux".source =
+    ./hatsune-miku-windows-linux-cursors/miku-cursor-linux;
+  xdg.dataFile."color-schemes/PlasmaOverdose.colors".source =
+    ./Plasma-Overdose/colorschemes/PlasmaOverdose.colors;
+  xdg.dataFile."sounds/Plasma-Overdose".source = ./Plasma-Overdose/sounds;
+  xdg.dataFile."aurorae/themes/Plasma-Overdose" = {
+    recursive = true;
+    source = ./Plasma-Overdose/aurorae/Plasma-Overdose;
   };
+  xdg.dataFile."plasma/look-and-feel/Plasma-Overdose".source =
+    ./Plasma-Overdose/plasma/look-and-feel/Plasma-Overdose;
 
   programs.plasma = {
     enable = true;
