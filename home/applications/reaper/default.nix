a5c2dff5daed1f274080a42bb0040e755682f2f3
l1npengtul <l1npengtul@protonmail.com>
2026-01-26 10:03:23 +0900

    l1npengtul@clubcyberia: Mon Jan 26 10:03:23 AM JST 2026 -

--- home/applications/reaper/default.nix
+++ home/applications/reaper/default.nix
@@ -2,41 +2,46 @@
   inputs,
   pkgs,
   ...
-}: {
+}: let
+  reapkgs = with inputs.reapkgs-known.legacyPackages.${pkgs.stdenv.hostPlatform.system}; [
+    birdbird-reascript-testing."BirdBird_Global Sampler.lua"
+    birdbird-reascript-testing."BirdBird_Parameter History.lua"
+    birdbird-reascript-testing."BirdBird_FX Inspector.lua"
+    rejj."ReEQ.jsfx"
+    saike-tools."Amaranth.jsfx"
+    saike-tools."saike_abyss.jsfx"
+    saike-tools."Saike_Morph.jsfx"
+    saike-tools."saike_smooth.jsfx"
+    saike-tools."saike_duskverb.jsfx"
+    saike-tools."saike_nostalgizer.jsfx"
+    saike-tools."saikedrums.jsfx"
+    saike-tools."saike_partials.jsfx"
+    saike-tools."saike_bric_a_brac.jsfx"
+    saike-tools."Swellotron.jsfx"
+    saike-tools."Reflectosaurus.jsfx"
+    saike-tools."Saike_FMFilter2.jsfx"
+    saike-tools."saike_lava.jsfx"
+    saike-tools."SatanVerb.jsfx"
+    saike-tools."Filther.jsfx"
+    saike-tools."StereoSpectrumSplit.jsfx"
+    saike-tools."StereoManipulator.jsfx"
+    saike-tools."SaikeMultiSpectralAnalyzer_MK2.jsfx"
+    saike-tools."Saike_Yutani.jsfx"
+    saike-tools."Saike Stereo Bub III.jsfx"
+    saike-tools."Transience.jsfx"
+    saike-tools."Tight_Compressor.jsfx"
+    saike-tools."ToneStacks.jsfx"
+    saike-tools."BandSplitter.jsfx"
+  ];
+  extras = with inputs.reapkgs-extras.legacyPackages.${pkgs.stdenv.hostPlatform.system}; [
+    demian-d."AD Collection.jsfx"
+  ];
+in {
   xdg.configFile.REAPER = {
     recursive = true;
     source = pkgs.symlinkJoin {
       name = "reapkgs";
-      paths = with inputs.reapkgs-known.legacyPackages.${pkgs.stdenv.hostPlatform.system}; [
-        birdbird-reascript-testing."BirdBird_Global Sampler.lua"
-        birdbird-reascript-testing."BirdBird_Parameter History.lua"
-        birdbird-reascript-testing."BirdBird_FX Inspector.lua"
-        rejj."ReEQ.jsfx"
-        saike-tools."Amaranth.jsfx"
-        saike-tools."saike_abyss.jsfx"
-        saike-tools."Saike_Morph.jsfx"
-        saike-tools."saike_smooth.jsfx"
-        saike-tools."saike_duskverb.jsfx"
-        saike-tools."saike_nostalgizer.jsfx"
-        saike-tools."saikedrums.jsfx"
-        saike-tools."saike_partials.jsfx"
-        saike-tools."saike_bric_a_brac.jsfx"
-        saike-tools."Swellotron.jsfx"
-        saike-tools."Reflectosaurus.jsfx"
-        saike-tools."Saike_FMFilter2.jsfx"
-        saike-tools."saike_lava.jsfx"
-        saike-tools."SatanVerb.jsfx"
-        saike-tools."Filther.jsfx"
-        saike-tools."StereoSpectrumSplit.jsfx"
-        saike-tools."StereoManipulator.jsfx"
-        saike-tools."SaikeMultiSpectralAnalyzer_MK2.jsfx"
-        saike-tools."Saike_Yutani.jsfx"
-        saike-tools."Saike Stereo Bub III.jsfx"
-        saike-tools."Transience.jsfx"
-        saike-tools."Tight_Compressor.jsfx"
-        saike-tools."ToneStacks.jsfx"
-        saike-tools."BandSplitter.jsfx"
-      ];
+      paths = reapkgs ++ extras;
     };
   };
 
