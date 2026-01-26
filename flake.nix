cd26d4fd38919b7c6534eeff98b20b85f6f16c36
l1npengtul <l1npengtul@protonmail.com>
2026-01-27 07:02:22 +0900

    l1npengtul@clubcyberia: Tue Jan 27 07:02:22 AM JST 2026 -

--- flake.nix
+++ flake.nix
@@ -130,7 +130,7 @@
             plasma-manager.homeModules.plasma-manager
             sops-nix.homeManagerModules.sops
             {
-              age.sshKeyFiles = ["/etc/ssh/ssh_host_ed25519_key"];
+              sops.age.sshKeyFiles = ["/etc/ssh/ssh_host_ed25519_key"];
             }
           ];
           users."${username}".imports = [
