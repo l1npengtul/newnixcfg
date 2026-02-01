29021945c28bb08a7bdf8cad8756fe1f28931217
l1npengtul <l1npengtul@protonmail.com>
2026-02-01 21:43:08 +0900

    l1npengtul@clubcyberia: Sun Feb  1 09:43:08 PM JST 2026 -

--- switch.sh
+++ switch.sh
@@ -13,6 +13,7 @@ git commit  --allow-empty -m "$(whoami)@${WHERE}: $(date) - $@"
 
 PK=$(sudo cat /etc/ssh/ssh_host_ed25519_key)
 echo ${PK} >> tmp.txt
+chmod 600 tmp.txt
 export SOPS_AGE_KEY=$(ssh-to-age -i tmp.txt -private-key)
 rm tmp.txt
 SSH_TO_AGE_PASSPHRASE=""
