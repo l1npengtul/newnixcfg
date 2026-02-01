e71a62c0fcca6128bb70b834f92fa1e51cdc7327
l1npengtul <l1npengtul@protonmail.com>
2026-02-01 21:39:50 +0900

    l1npengtul@clubcyberia: Sun Feb  1 09:39:50 PM JST 2026 -

--- switch.sh
+++ switch.sh
@@ -12,7 +12,7 @@ git add .
 git commit  --allow-empty -m "$(whoami)@${WHERE}: $(date) - $@"
 
 PK=$(sudo cat /etc/ssh/ssh_host_ed25519_key)
-export SOPS_AGE_KEY=$(ssh-to-age ${PK} -private-key)
+export SOPS_AGE_KEY=$(echo ${PK} | ssh-to-age -private-key)
 SSH_TO_AGE_PASSPHRASE=""
 PK=""
 
