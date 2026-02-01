897d6a1b93ddd8419d50f073875897992eb0bd4d
l1npengtul <l1npengtul@protonmail.com>
2026-02-01 21:44:25 +0900

    l1npengtul@clubcyberia: Sun Feb  1 09:44:25 PM JST 2026 -

--- switch.sh
+++ switch.sh
@@ -11,7 +11,7 @@ git add .
 
 git commit  --allow-empty -m "$(whoami)@${WHERE}: $(date) - $@"
 
-export SOPS_AGE_KEY=$(sudo ssh-to-age -i ed25519_key -private-key)
+export SOPS_AGE_KEY=$(sudo ssh-to-age -i /etc/ssh/ssh_host_ed25519_key -private-key)
 
 nixos-rebuild build --show-trace --builders "" --flake .#$WHERE
 
