9a08bbc8d4344361d45e103ce313da58e07f0cf7
l1npengtul <l1npengtul@protonmail.com>
2026-02-05 21:48:02 +0900

    l1npengtul@clubcyberia: Thu Feb  5 09:48:02 PM JST 2026 -

--- services/syncthing-base.nix
+++ services/syncthing-base.nix
@@ -6,25 +6,12 @@
   shhh = builtins.toString inputs.shhh;
   syc = inputs.shhh.services.syncthing;
 in {
-  sops.secrets."syncthing/key" = {
-    sopsFile = "${shhh}/${config.networking.hostName}.yaml";
-  };
-  sops.secrets."syncthing/cert" = {
-    sopsFile = "${shhh}/${config.networking.hostName}.yaml";
-  };
-  sops.secrets."syncthing/password" = {
-    sopsFile = "${shhh}/syncthing.yaml";
-  };
-  sops.secrets."syncthing/decrypt" = {
-    sopsFile = "${shhh}/syncthing.yaml";
-  };
-
   services.syncthing = {
     enable = true;
     overrideDevices = true;
     overrideFolders = true;
-    key = config.sops.secrets."syncthing/key".path;
-    cert = config.sops.secrets."syncthing/cert".path;
+    key = config.sops.secrets."syncthing-key".path;
+    cert = config.sops.secrets."syncthing-cert".path;
     user = username;
     guiAddress = syc.gui-port;
     guiPasswordFile = config.sops.secrets."syncthing/password".path;
@@ -40,5 +27,18 @@ in {
         user = username;
       };
     };
+  };
+
+  sops.secrets."syncthing-key" = {
+    sopsFile = "${shhh}/${config.networking.hostName}.yaml";
+  };
+  sops.secrets."syncthing-cert" = {
+    sopsFile = "${shhh}/${config.networking.hostName}.yaml";
+  };
+  sops.secrets."syncthing/password" = {
+    sopsFile = "${shhh}/syncthing.yaml";
+  };
+  sops.secrets."syncthing/decrypt" = {
+    sopsFile = "${shhh}/syncthing.yaml";
   };
 }
