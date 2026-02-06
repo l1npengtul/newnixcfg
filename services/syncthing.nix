0f84c55f49d326554dbd03c8e8a3805f00afb8dc
l1npengtul <l1npengtul@protonmail.com>
2026-02-07 08:37:47 +0900

    l1npengtul@clubcyberia: Sat Feb  7 08:37:47 AM JST 2026 -

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -2,10 +2,25 @@
   inputs,
   config,
   ...
-}: {
+}: let
+  shhh = builtins.toString inputs.shhh;
+in {
   import = [
     (import ./syncthing-base.nix {
       inherit inputs config;
     })
   ];
+
+  sops.secrets."syncthing-key" = {
+    sopsFile = "${shhh}/${config.networking.hostName}.yaml";
+  };
+  sops.secrets."syncthing-cert" = {
+    sopsFile = "${shhh}/${config.networking.hostName}.yaml";
+  };
+  sops.secrets."syncthing/password" = {
+    sopsFile = "${shhh}/syncthing.yaml";
+  };
+  sops.secrets."syncthing/decrypt" = {
+    sopsFile = "${shhh}/syncthing.yaml";
+  };
 }
